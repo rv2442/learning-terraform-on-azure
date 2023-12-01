a93b92dbb44bfdc17deede7b946a1cbcb60f07bb
Rahul Vijan <rahulvijan1@gmail.com>
2023-12-01 18:35:10 +0530

    deploying a vnet

--- main.tf
+++ main.tf
@@ -19,3 +19,10 @@ resource "azurerm_resource_group" "main" {
   name     = "learn-tf-rg-eastus"
   location = "eastus"
 }
+
+resource "azurerm_virtual_network" "name" {
+  name = "learn-tf-vnet"
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space = ["10.0.0.0/16"]
+}
